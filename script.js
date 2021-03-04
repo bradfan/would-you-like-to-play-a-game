da4442b64d24ce58007e088edf3e34720192fefb
jcerb <brt.jst@gmail.com>
2021-03-04 16:32:42 -0700

    save before major rewrite

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+// this starts the function when clicked
+var startButton = document.querySelector("#start");
+var clock = document.querySelector("#clock");
+// HIDES CONTAINERS AT OPEN SCREEN
+var openScreen = document.querySelector("body");
+var qcontainer1 = document.querySelector("#qcontainer1");
+var qcontainer2 = document.querySelector("#qcontainer2");
+var qcontainer3 = document.querySelector("#qcontainer3");
 // how many seconds in game GV
 var time = 100;
 // sets up array for score
-var tally = ""
+var tally = "";
 // question and answer array (key: value) GV
-var question1 = ["blah"]
-var question2 = ["blah"] 
-var question3 = ["blah"]
-
-// HIDEs QCONTAINERS AT OPEN SCREEN
-   var openScreen = document.querySelector("body")
-    openScreen.addEventListener("", function(){
-    qcontainer1.style.display = "none";
-    qcontainer2.style.display = "none";
-    qcontainer3.style.display = "none";
-})
-
-// // this starts the function when clicked
-var startButton = document.querySelector("#start");
-var clock = document.querySelector("#clock");
-startButton.addEventListener("click", function (e) {
-  startQuestion();
-  would.style.display ="none";
+var question1 = "Who is the inventor of JavaScript?";
+var question2 = "blah";
+var question3 = "blah";
+window.addEventListener("load", function (event) {
+  qcontainer1.style.display = "none";
+  qcontainer2.style.display = "none";
+  qcontainer3.style.display = "none";
+});
+startButton.addEventListener("click", function (event) {
+  startButton.style.display = "none";
+  would.style.display = "none";
   words.style.display = "none";
-  qcontainer1.style.display = "inherit"
-  clock.textContent = time;
+  document.querySelector("#questiontext1").textContent = question1;
+  qcontainer1.style.display ="block"
+  event.target.getAttribute("id")
+   clock.textContent = time;
   var clockMsg = setInterval(function () {
     time--;
     clock.textContent = time;
@@ -31,68 +34,46 @@ startButton.addEventListener("click", function (e) {
       clearInterval(clockMsg);
     }
   }, 1000);
- }
-);
+  console.log(event.target.getAttribute("id"))
 
+});
 // FUNCTIONS BELOW MOVE FROM ONE TO ANOTHER DISPLAYING QCONTAINERS IN SEQUENCE
-var qcontainer1 = document.querySelector("#qcontainer1");
-startQuestion.addEventListener("click",function(e) {
-    qcontainer1.textContent = question1[0]
-    secondQuestion()    
-    e.target.getAttribute("class")
-})
-
-addEventListener("click",startQuestion)
-var qcontainer2 = document.querySelector("#qcontainer2");
-
-function secondQuestion() {
-  qcontainer1.textContent = question2[0]
-  qcontainer2.style.display = "inherit";   
+// NEEDS EVENTLISTENER
+  function secondQuestion(event) {
+  event.target.getAttribute("id")
+  qcontainer1.style.display = "none";
+  qcontainer2.textContent = question2;
+  qcontainer2.style.display = "block";
+  // document.querySelector("#questiontext2").textContent = question2;
+  console.log(event.target.getAttribute("id"))
 }
-
-addEventListener("click",secondQuestion)
-var qcontainer3 = document.querySelector("#qcontainer2");
+// NEEDS EVENTLISTENER
 function lastQuestion() {
-  qcontainer1.textContent = question3[0]
-  qcontainer3.style.display = "inherit";     
+  qcontainer1.textContent = question3;
+  qcontainer3.style.display = "block";
+  document.querySelector("#questiontext1").textContent = question1;
+
 }
-
-
-    //or add the button html dynamically here
-    // for all 4 buttons
-    // for button text for button with id a as  myQuestions[0].options[0]
-    //  var btna = document.createElement("");
-    // .type = "";
-    // btna.innerText = "A"
-    // document.qcontainer1.appendChild(btna);
-    
-
-// var answerBtn = document.querySelector(".answer"); 
+//or add the button html dynamically here
+// for all 4 buttons
+// for button text for button with id a as  myQuestions[0].options[0]
+//  var btna = document.createElement("");
+// .type = "";
+// btna.innerText = "A"
+// document.qcontainer1.appendChild(btna);
+// var answerBtn = document.querySelector(".answer");
 // answerBtn.addEventListener("click", function(event){
 //   var element = event.target;
-
 //   if (element.matches("")){
 //     var state = element.getAttribute("data-state");
 //     if(state ===""){
-
 //     }
-
 //   }
-  
 // })
-// console.log(event.target.getAttribute("id")
-
+// console.log(event.target.getAttribute("id"))
 // TAKE SYNTAX OUT OF MESSAGE AS NEEDED
-
-
-
 // THIS WILL POPULATE THE FUNCTION THAT STORES ANSWERS
 // var answers = document.querySelector("#answers");
-
-
-
-
-
 // GIVE ANSWERS (FOR EACH LOOP?) OR FOR LOOP
 //      questions [0].answers.forEach(function(item){
 //         //  create html button
@@ -101,10 +82,8 @@ function lastQuestion() {
 //          answer.textContent = item;
 //         //  append it to our options div
 //          options.append(answerBtn);
-
 //      })
 // })
-
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
@@ -116,3 +95,4 @@ function lastQuestion() {
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
